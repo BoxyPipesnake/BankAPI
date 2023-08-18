85e6aef438d3fd54ddd56865871682b243503737
BoxyPipesnake <felipe_motoss@hotmail.com>
2023-08-18 16:25:30 -0600

    Creacion del BankTransactionController

---
+++ Controllers/BankTransactionController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using BankAPI.Services;
+using BankAPI.Data.BankModels;
+
+namespace BankAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BankTransactionController : ControllerBase
+{
+
+}
